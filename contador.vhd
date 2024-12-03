c68193c3948417fb1a9411cebdcb639cefc2364f
Victor Garrett <victor.garrett@driva.com.br>
2024-12-03 18:39:22 -0300

    auauuad

--- contador.vhd
+++ contador.vhd
@@ -13,7 +13,6 @@ entity contador is
 			rst: in std_logic;
 			en: in std_logic;
 			clr: in std_logic;
-			ld: in std_logic
 	);
 	
 end contador;
@@ -53,12 +52,14 @@ begin
 	
 	cu_is_top <= out_cu_s(3) and not out_cu_s(2) and not out_cu_s(1) and out_cu_s(0);
 	cd_is_top <= out_cd_s(3) and not out_cd_s(2) and not out_cd_s(1) and out_cd_s(0);
+	su_is_top <= out_su_s(3) and not out_su_s(2) and not out_su_s(1) and out_su_s(0);
+	sd_is_top <= not out_sd_s(3) and out_sd_s(2) and not out_sd_s(1) and out_sd_s(0);
 
 	counter_cu: cont_4b port map (rst=>rst, clk=>clk, en=>en, clr=>clr or cu_is_top, q=>out_cu_s, ld=>0, load=>"0000");
 	counter_cd: cont_4b port map (rst=>rst, clk=>clk, en=>en and u_is_top, clr=>clr or (cu_is_top and cd_is_top), q=>out_cd_s, ld=>0, load=>"0000");
 
 	
-	counter_su: cont_4b port map (rst=>rst, clk=>clk, en=>en and cd_is_top and cu_is_top, clr=>clr or su_is_top, q=>out_su_s, ld=>ld or internal_ld, load=>"0000");
-	counter_sd: cont_4b port map (rst=>rst, clk=>clk, en=>en ), clr=>clr, q=>out_sd_s, ld=>0, load=>"0000");
+	counter_su: cont_4b port map (rst=>rst, clk=>clk, en=>en and cd_is_top and cu_is_top, clr=>clr or su_is_top, q=>out_su_s, ld=>0, load=>"0000");
+	counter_sd: cont_4b port map (rst=>rst, clk=>clk, en=>en and su_is_top, clr=>clr or (su_is_top and sd_is_top), q=>out_sd_s, ld=>0, load=>"0000");
 
 end architecture;
--- crono.vhd
+++ crono.vhd
@@ -2,63 +2,68 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity contador is
-	port( out_u: out unsigned(3 downto 0);
-			out_d: out unsigned(3 downto 0);
+entity crono is
+	port(   out_su_disp: out unsigned(6 downto 0);
+			out_sd_disp: out unsigned(6 downto 0);
 			
-			load_u: in unsigned(3 downto 0);
-			load_d: in unsigned(3 downto 0);
+			out_cu_disp: out unsigned(6 downto 0);
+			out_cd_disp: out unsigned(6 downto 0);
+			
+			clk: in std_logic;
+			rst: in std_logic;
+			run_btn: in std_logic;
+			clr_btn: in std_logic
+	);
+	
+end crono;
+
+architecture arch of crono is
+
+	component contador
+		port(
+			out_su: out unsigned(3 downto 0);
+			out_sd: out unsigned(3 downto 0);
+			
+			out_cu: out unsigned(3 downto 0);
+			out_cd: out unsigned(3 downto 0);
 			
 			clk: in std_logic;
 			rst: in std_logic;
 			en: in std_logic;
 			clr: in std_logic;
-			ld: in std_logic
-	);
-	
-end contador;
-
-architecture arch of contador is
-
-	component cont_4b
-		port(
-				rst: in std_logic;
-				clk: in std_logic;
-				q: out unsigned(3 downto 0);
-				en: in std_logic;
-				clr: in std_logic;
-				ld: in std_logic;
-				load: in unsigned(3 downto 0)
 		);
 	end component;
 
-	signal out_u_s: unsigned(3 downto 0);
-	signal out_d_s: unsigned(3 downto 0);
+	component hex_to_7_seg
+		port(
+			hex_in: in unsigned(3 downto 0);
+			7_seg_out: out unsigned(6 downto 0);
+		);
+	end component;
 	
-	signal u_is_top: std_logic;
-	signal internal_ld: std_logic;
 
-	signal internal_load_u: unsigned(3 downto 0);
-	signal internal_load_d: unsigned(3 downto 0);
-	
-	
-	
+	signal out_su_s: unsigned(3 downto 0);
+	signal out_sd_s: unsigned(3 downto 0);
+	signal out_cu_s: unsigned(3 downto 0);
+	signal out_cd_s: unsigned(3 downto 0);
+
+	signal is_running: std_logic;
 begin
 	
-	
-	out_u <= out_u_s;
-	out_d <= out_d_s;
-	
-	internal_load_u <= "0010" when ld = '0' else load_u;
-	internal_load_d <= "0001" when ld = '0' else load_d;
-	
-	internal_ld <=  out_u_s(3) and not out_u_s(2) and not out_u_s(1) and not out_u_s(0) and
-						 not out_d_s(3) and out_d_s(2) and out_d_s(1) and not out_d_s(0);
-	
-	u_is_top <= out_u_s(3) and not out_u_s(2) and not out_u_s(1) and out_u_s(0);
+	cont: port map contador(out_su=>out_su_s, out_sd=>out_sd_s, out_cu=>out_cu_s, out_cd=>out_cd_s, clk=>clk, rst=>rst, en=>is_running, clr=>(clr_btn and not is_running))
 
-	counter_u: cont_4b port map (rst=>rst, clk=>clk, en=>en, clr=>clr or u_is_top, q=>out_u_s, ld=>ld or internal_ld, load=>internal_load_u);
-	counter_d: cont_4b port map (rst=>rst, clk=>clk, en=>en and (u_is_top or internal_ld or ld), clr=>clr, q=>out_d_s, ld=>ld or internal_ld, load=>internal_load_d);
+	process(clk, run_btn)
+	begin
+		if rising_edge(clk)
+			if run_btn = 0
+				is_running <= not is_running;
+			end if;
+		end if;
+	end process;
 
+	conv_su: port map hex_to_7_seg(hex_in=>out_su_s, 7_seg_out=>out_su_disp)
+	conv_sd: port map hex_to_7_seg(hex_in=>out_sd_s, 7_seg_out=>out_sd_disp)
+	conv_cu: port map hex_to_7_seg(hex_in=>out_cu_s, 7_seg_out=>out_cu_disp)
+	conv_cd: port map hex_to_7_seg(hex_in=>out_cd_s, 7_seg_out=>out_cd_disp)
 
 end architecture;
